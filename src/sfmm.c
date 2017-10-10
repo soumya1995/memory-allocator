b208f562ca5de82c3bdea9f4436b72b46b01c6c6
Soumya Das <sodas@cs.stonybrook.edu>
2017-10-10 08:09:31 -0400

    malloc() written; implementing helper functions for malloc()

--- src/sfmm.c
+++ src/sfmm.c
@@ -6,6 +6,7 @@
 #include "sfmm.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "mm.h"
 
 /**
  * You should store the heads of your free lists in these variables.
@@ -22,7 +23,70 @@ free_list seg_free_list[4] = {
 int sf_errno = 0;
 
 void *sf_malloc(size_t size) {
+
+    size_t asize;  /*ADJUSTED BLOCK SIZE*/
+   // size_t extendsize;  /*EXTEND HEAP AMMOUNT WHEN NO FIT FOUND*/
+    char *bp;
+
+    /*IGNORE IF SIZE IS ZERO OR GREATER THAN PAGE SIZE*/
+    if(size == 0 || size>PAGE){
+        sf_errno =  EINVAL;
+        return NULL;
+    }
+
+    //ADJUST THE BLACK SIZE AND ALIGNMENT
+    if(size <= MEMALIGN)
+        asize = 2*MEMROW + MEMALIGN;
+    else
+        asize = (((size+MEMALIGN-1)/MEMALIGN)*MEMALIGN) + (2*MEMROW);
+
+    //SEARCH FREE LISTS FOR A FIT
+    if((bp = find_fit(asize))!= NULL){
+        place(bp, asize);
+        return bp;
+    }
+
+    //NO FIT FOUND. EXTEND THE HEAP TO GET MORE MEMORY
+    //extendsize = MAX(asize, CHUNKSIZE);
+    //if((bp = extend_heap(extendsize)) == NULL)
+        extend_heap();
+        return NULL;
+    place(bp, asize);
+    return bp;
+
+
 	return NULL;
+}
+
+void *extend_heap(){
+
+    char *bp;
+    size_t size = PAGE;
+
+    if((long)(bp = sf_sbrk()) == -1)
+        return NULL;
+
+    PUT(HDRP(bp), PACK(size, 0)); /*FREE BLOCK HEADER*/
+    PUT(FTRP(bp), PACK(size, 0));   /*FREE BLOCK FOOTER*/
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0,1));  /*NEW EPILOGUE HEADER*/
+
+    /*COLEASE IF THE PREVIOUS BLOCK WAS FREE*/
+    return coalesce(bp);
+
+}
+
+void *coalesce(void *bp, int mode){
+
+size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+size_t
+
+    /*mode IS 0 COLEASE WITH LOWER ADDRESS AND mode IS 1 COLEASE WITH HIGHER ADDRESS*/
+    if(mode == 0){
+
+    }
+
+
 }
 
 void *sf_realloc(void *ptr, size_t size) {
